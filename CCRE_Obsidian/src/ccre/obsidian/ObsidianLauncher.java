b15d6e4d711ad9ea029c5d462900459bf700f81b
skeggsc <skeggsc@catlin.edu>
2013-09-27 15:34:52 -0700

    Added basic parts for CCRE Obsidian - the BeagleBone Black port that will be used in our InventTeams branch. Fixed documentation incompatibility caused by previous commit. Made xmltask.jar copy in local directory.

---
+++ CCRE_Obsidian/src/ccre/obsidian/ObsidianLauncher.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ccre.obsidian;
+
+/**
+ *
+ * @author skeggsc
+ */
+class ObsidianLauncher {
+
+}

8380a235126616f1f10f4e8379c0f669094b7bce
Vincent Miller <millerv@catlin.edu>
2013-10-08 15:32:41 -0700

    Post-merge commit.

--- First_Project/src/test/Test.java
+++ First_Project/src/test/Test.java
@@ -5,23 +5,21 @@ import ccre.chan.FloatOutput;
 import ccre.event.EventConsumer;
 import ccre.igneous.SimpleCore;
 
-// Most main Igneous applications will extend SimpleCore.
-// Don't change this unless you know what you are doing.
 public class Test extends SimpleCore {
 
     protected void createSimpleControl() {
 
 
-        final FloatOutput leftMotorOutput = makeTalonMotor(1, MOTOR_FORWARD);
-        final FloatOutput rightMotorOutput = makeTalonMotor(2, MOTOR_REVERSE);
+        final FloatOutput leftMotorOutput = makeTalonMotor(2, MOTOR_FORWARD);
+        final FloatOutput rightMotorOutput = makeTalonMotor(1, MOTOR_FORWARD);
 
         final FloatInputPoll joystickXAxis = joystick1.getXChannel();
         final FloatInputPoll joystickYAxis = joystick1.getYChannel();
 
         duringTeleop.addListener(new EventConsumer() {
             public void eventFired() {
-                leftMotorOutput.writeValue(joystickXAxis.readValue() + joystickYAxis.readValue());
-                rightMotorOutput.writeValue(-joystickXAxis.readValue() + joystickYAxis.readValue());
+                leftMotorOutput.writeValue(-joystickXAxis.readValue() + joystickYAxis.readValue());
+                rightMotorOutput.writeValue(joystickXAxis.readValue() + joystickYAxis.readValue());
             }
         });
     }

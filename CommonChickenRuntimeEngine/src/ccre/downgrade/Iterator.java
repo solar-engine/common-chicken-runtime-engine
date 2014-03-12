a96ffd623a2d3d1581a3614472999849905aa7d9
Gregor Peach <gregorpeach@gmail.com>
2014-03-11 18:12:20 -0700

    Fixed the issue with auto format.

--- CommonChickenRuntimeEngine/src/ccre/downgrade/Iterator.java
+++ CommonChickenRuntimeEngine/src/ccre/downgrade/Iterator.java
@@ -19,9 +19,9 @@
 package ccre.downgrade;
 
 /**
- * This is the same as java.util.Iterator. Don't use this. It is used when
- * Retrotranslator downgrades the code to 1.3, because 1.3 doesn't have
- * Iterator.
+ * This is the same as java.util.Iterator. Don't use
+ * this. It is used when Retrotranslator downgrades the code to 1.3, because 1.3
+ * doesn't have Iterator.
  *
  * @param <E> The type returned by the iterator.
  * @see java.util.Iterator

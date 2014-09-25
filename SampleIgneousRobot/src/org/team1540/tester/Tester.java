73bf3da66cc47727c7c96a33b365ca8c1dc27618
skeggsc <skeggsc@catlin.edu>
2014-09-25 15:39:10 -0700

    Fixed Tester to be compatible with both roboRIOs and cRIOs.

--- SampleIgneousRobot/src/org/team1540/tester/Tester.java
+++ SampleIgneousRobot/src/org/team1540/tester/Tester.java
@@ -21,19 +21,22 @@ public class Tester implements IgneousApplication {
      * Set up the robot. For the testing robot, this means publishing all the motors.
      */
     public void setupRobot() {
-        for (int i=0; i<20; i++) {
+        int base = Igneous.isRoboRIO() ? 0 : 1;
+        for (int i=base; i<20 + base; i++) {
             Cluck.publish("talon-" + i, Igneous.makeTalonMotor(i, false, 0.1f));
         }
-        for (int i=0; i<8; i++) {
+        for (int i=base; i<8 + base; i++) {
             Cluck.publish("solenoid-" + i, Igneous.makeSolenoid(i));
         }
-        for (int i=0; i<4; i++) {
+        for (int i=base; i<4 + base; i++) {
             Cluck.publish("analog-" + i, FloatMixing.createDispatch(Igneous.makeAnalogInput(i, 8), Igneous.globalPeriodic));
         }
-        for (int i=0; i<16; i++) {
-            Cluck.publish("current-" + i, FloatMixing.createDispatch(Igneous.getPDPChannelCurrent(i), Igneous.globalPeriodic));
+        if (Igneous.isRoboRIO()) {
+            for (int i=base; i<16 + base; i++) {
+                Cluck.publish("current-" + i, FloatMixing.createDispatch(Igneous.getPDPChannelCurrent(i), Igneous.globalPeriodic));
+            }
+            Cluck.publish("compressor", Igneous.usePCMCompressor());
+            Cluck.publish("pdp-voltage", FloatMixing.createDispatch(Igneous.getPDPVoltage(), Igneous.globalPeriodic));
         }
-        Cluck.publish("compressor", Igneous.usePCMCompressor());
-        Cluck.publish("pdp-voltage", FloatMixing.createDispatch(Igneous.getPDPVoltage(), Igneous.globalPeriodic));
     }
 }

4eb72f12a3e00dee257364cbc25f2bcb7021c86e
Skeggs <skeggsc@STU16-05.catlin.edu>
2014-10-03 11:02:06 -0700

    Fixed up build process for 2014 targets. Hopefully in a stable state. Not ready for prime time.

--- SampleIgneousRobot/src/org/team1540/tester/Tester.java
+++ SampleIgneousRobot/src/org/team1540/tester/Tester.java
@@ -1,38 +1,37 @@
 package org.team1540.tester;
 
-import ccre.channel.EventOutput;
 import ccre.cluck.Cluck;
 import ccre.ctrl.FloatMixing;
 import ccre.igneous.Igneous;
 import ccre.igneous.IgneousApplication;
-import ccre.log.Logger;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 /**
  * An example program that simply shares all the motors over the network.
  * 
- * Surprisingly useful for an example! Make sure to change which types the motors are.
+ * Surprisingly useful for an example! Make sure to change which types the
+ * motors are.
  *
  * @author skeggsc
  */
 public class Tester implements IgneousApplication {
 
     /**
-     * Set up the robot. For the testing robot, this means publishing all the motors.
+     * Set up the robot. For the testing robot, this means publishing all the
+     * motors.
      */
     public void setupRobot() {
         int base = Igneous.isRoboRIO() ? 0 : 1;
-        for (int i=base; i<20 + base; i++) {
+        for (int i = base; i < 20 + base; i++) {
             Cluck.publish("talon-" + i, Igneous.makeTalonMotor(i, false, 0.1f));
         }
-        for (int i=base; i<8 + base; i++) {
+        for (int i = base; i < 8 + base; i++) {
             Cluck.publish("solenoid-" + i, Igneous.makeSolenoid(i));
         }
-        for (int i=base; i<4 + base; i++) {
+        for (int i = base; i < 4 + base; i++) {
             Cluck.publish("analog-" + i, FloatMixing.createDispatch(Igneous.makeAnalogInput(i, 8), Igneous.globalPeriodic));
         }
         if (Igneous.isRoboRIO()) {
-            for (int i=base; i<16 + base; i++) {
+            for (int i = base; i < 16 + base; i++) {
                 Cluck.publish("current-" + i, FloatMixing.createDispatch(Igneous.getPDPChannelCurrent(i), Igneous.globalPeriodic));
             }
             Cluck.publish("compressor", Igneous.usePCMCompressor());

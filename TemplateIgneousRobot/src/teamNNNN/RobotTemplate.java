284ef88446ef10c23552ba0b7b7d51cf05d3e3bd
skeggsc <skeggsc@catlin.edu>
2014-10-13 15:43:58 -0700

    Added TemplateIgneousRobot to be used as a template instead of SampleIgneousRobot.

---
+++ TemplateIgneousRobot/src/teamNNNN/RobotTemplate.java
@@ -0,0 +1,11 @@
+package teamNNNN;
+
+import ccre.igneous.Igneous;
+import ccre.igneous.IgneousApplication;
+
+public class RobotTemplate implements IgneousApplication {
+
+    public void setupRobot() {
+        // Robot setup code goes here.
+    }
+}

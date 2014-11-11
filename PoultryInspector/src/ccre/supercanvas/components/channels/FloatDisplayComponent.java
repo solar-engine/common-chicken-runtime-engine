881d1beb2cd077712f60a04ecee3a80c32ce1fcc
Colby Skeggs <skeggsc@catlin.edu>
2014-11-10 19:39:46 -0800

    Final set of control alternatives for the base implementation.

--- PoultryInspector/src/ccre/supercanvas/components/channels/FloatDisplayComponent.java
+++ PoultryInspector/src/ccre/supercanvas/components/channels/FloatDisplayComponent.java
@@ -18,15 +18,21 @@
  */
 package ccre.supercanvas.components.channels;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.Shape;
+import java.awt.Stroke;
+import java.awt.geom.AffineTransform;
 
 import ccre.channel.FloatInput;
 import ccre.channel.FloatOutput;
+import ccre.log.Logger;
 import ccre.supercanvas.BaseChannelComponent;
+import ccre.supercanvas.Rendering;
 import ccre.supercanvas.SuperCanvasPanel;
 
 /**
@@ -37,7 +43,7 @@ import ccre.supercanvas.SuperCanvasPanel;
 public class FloatDisplayComponent extends BaseChannelComponent<FloatDisplayComponent.View> implements FloatOutput {
 
     public static enum View {
-        CONFIGURATION, HORIZONTAL_POINTER
+        CONFIGURATION, HORIZONTAL_POINTER, DIAL, TEXTUAL
     }
 
     private static final long serialVersionUID = 4027452153991095626L;
@@ -71,38 +77,110 @@ public class FloatDisplayComponent extends BaseChannelComponent<FloatDisplayComp
 
     @Override
     public void channelRender(Graphics2D g, int screenWidth, int screenHeight, FontMetrics fontMetrics, int mouseX, int mouseY) {
-        g.setColor(Color.WHITE);
-        g.fillRect(centerX - halfWidth + 10, centerY - halfHeight / 2, 2 * halfWidth - 20, halfHeight);
-        g.setColor(Color.BLACK);
-        g.drawRect(centerX - halfWidth + 10, centerY - halfHeight / 2, 2 * halfWidth - 21, halfHeight - 1);
-        g.drawLine(centerX, centerY + halfHeight / 2 - 1, centerX, centerY + 5);
-        g.drawLine(centerX + halfWidth * 2 / 3, centerY + halfHeight / 2 - 1, centerX + halfWidth * 2 / 3, centerY + 5);
-        g.drawLine(centerX - halfWidth * 2 / 3, centerY + halfHeight / 2 - 1, centerX - halfWidth * 2 / 3, centerY + 5);
-        g.drawLine(centerX - halfWidth / 6, centerY + halfHeight / 2 - 1, centerX - halfWidth / 6, centerY + 15);
-        g.drawLine(centerX + halfWidth / 6, centerY + halfHeight / 2 - 1, centerX + halfWidth / 6, centerY + 15);
-        g.drawLine(centerX - halfWidth / 3, centerY + halfHeight / 2 - 1, centerX - halfWidth / 3, centerY + 10);
-        g.drawLine(centerX + halfWidth / 3, centerY + halfHeight / 2 - 1, centerX + halfWidth / 3, centerY + 10);
-        g.drawLine(centerX - 3 * halfWidth / 6, centerY + halfHeight / 2 - 1, centerX - 3 * halfWidth / 6, centerY + 15);
-        g.drawLine(centerX + 3 * halfWidth / 6, centerY + halfHeight / 2 - 1, centerX + 3 * halfWidth / 6, centerY + 15);
-        if (value != 0) {
-            String strv = String.format("%.3f", value);
-            g.drawString(strv, value > 0 ? centerX - fontMetrics.stringWidth(strv) - 10 : centerX + 10, centerY - halfHeight / 2 + fontMetrics.getHeight());
-        }
-        int ptrCtr = centerX + (int) (halfWidth * 2 / 3 * value);
-        if (Math.abs(value) <= 1) {
-            if (value < 0) {
-                g.setColor(value == -1 ? Color.RED : Color.RED.darker().darker());
-            } else if (value > 0) {
-                g.setColor(value == 1 ? Color.GREEN : Color.GREEN.darker().darker());
-            } else {
-                g.setColor(Color.ORANGE);
+        switch (activeView) {
+        case HORIZONTAL_POINTER:
+            g.setColor(Color.WHITE);
+            g.fillRect(centerX - halfWidth + 10, centerY - halfHeight / 2, 2 * halfWidth - 20, halfHeight);
+            g.setColor(Color.BLACK);
+            g.drawRect(centerX - halfWidth + 10, centerY - halfHeight / 2, 2 * halfWidth - 21, halfHeight - 1);
+            g.drawLine(centerX, centerY + halfHeight / 2 - 1, centerX, centerY + 5);
+            g.drawLine(centerX + halfWidth * 2 / 3, centerY + halfHeight / 2 - 1, centerX + halfWidth * 2 / 3, centerY + 5);
+            g.drawLine(centerX - halfWidth * 2 / 3, centerY + halfHeight / 2 - 1, centerX - halfWidth * 2 / 3, centerY + 5);
+            g.drawLine(centerX - halfWidth / 6, centerY + halfHeight / 2 - 1, centerX - halfWidth / 6, centerY + 15);
+            g.drawLine(centerX + halfWidth / 6, centerY + halfHeight / 2 - 1, centerX + halfWidth / 6, centerY + 15);
+            g.drawLine(centerX - halfWidth / 3, centerY + halfHeight / 2 - 1, centerX - halfWidth / 3, centerY + 10);
+            g.drawLine(centerX + halfWidth / 3, centerY + halfHeight / 2 - 1, centerX + halfWidth / 3, centerY + 10);
+            g.drawLine(centerX - 3 * halfWidth / 6, centerY + halfHeight / 2 - 1, centerX - 3 * halfWidth / 6, centerY + 15);
+            g.drawLine(centerX + 3 * halfWidth / 6, centerY + halfHeight / 2 - 1, centerX + 3 * halfWidth / 6, centerY + 15);
+            if (value != 0) {
+                String strv = String.format("%.3f", value);
+                g.drawString(strv, value > 0 ? centerX - fontMetrics.stringWidth(strv) - 10 : centerX + 10, centerY - halfHeight / 2 + fontMetrics.getHeight());
             }
+            int ptrCtr = centerX + (int) (halfWidth * 2 / 3 * value);
+            if (Math.abs(value) <= 1) {
+                if (value < 0) {
+                    g.setColor(value == -1 ? Color.RED : Color.RED.darker().darker());
+                } else if (value > 0) {
+                    g.setColor(value == 1 ? Color.GREEN : Color.GREEN.darker().darker());
+                } else {
+                    g.setColor(Color.ORANGE);
+                }
+            }
+            Shape c = g.getClip();
+            g.setClip(new Rectangle(centerX - halfWidth + 10, centerY - halfHeight / 2, halfWidth * 2 - 20, halfHeight));
+            g.fillRect(ptrCtr - 5, centerY - halfHeight / 2 + 1, 11, halfHeight / 2 - 4);
+            g.fillPolygon(new int[] { ptrCtr - 5, ptrCtr, ptrCtr + 6 }, new int[] { centerY - 3, centerY + 3, centerY - 3 }, 3);
+            g.setClip(c);
+            break;
+        case DIAL:
+            g.setColor(Color.WHITE);
+            int rad = halfWidth - 8;
+            g.fillOval(centerX - halfWidth + 8, centerY - halfHeight + 20, rad * 2, rad * 2);
+            g.setColor(Color.BLACK);
+            g.drawOval(centerX - halfWidth + 8, centerY - halfHeight + 20, rad * 2, rad * 2);
+            g.setColor(Color.BLACK);
+            AffineTransform origT = g.getTransform();
+            g.translate(centerX, centerY - halfHeight + 20 + rad);
+            AffineTransform baseT = g.getTransform();
+            int eachSpoke = 14;
+            g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+            for (int i=-10 * eachSpoke; i<=10 * eachSpoke; i += eachSpoke) {
+                g.setTransform(baseT);
+                g.rotate(Math.toRadians(i));
+                g.translate(0, 1-rad);
+                if (i % (eachSpoke * 5) == 0) {
+                    g.drawLine(0, 0, 0, rad/3);
+                    g.translate(0, rad / 3f);
+                    g.rotate(Math.toRadians(i > 0 ? -90 : 90));
+                    String str = Float.toString(i / (10f * eachSpoke));
+                    g.drawString(str, i > 0 ? -g.getFontMetrics().stringWidth(str) : 0, g.getFontMetrics().getDescent());
+                } else {
+                    g.drawLine(0, 0, 0, rad/6);
+                }
+            }
+            g.setTransform(baseT);
+            float angle = Math.max(-170, Math.min(value * 10 * eachSpoke, 170));
+            g.setColor(Color.BLUE);
+            if (angle <= -170 || angle >= 170) {
+                g.setColor(Color.RED);
+            }
+            g.rotate(Math.toRadians(angle));
+            Stroke origS = g.getStroke();
+            g.setStroke(new BasicStroke(6, BasicStroke.CAP_ROUND, BasicStroke.CAP_SQUARE));
+            g.drawLine(0, -rad/2, 0, 0);
+            g.setStroke(origS);
+            g.setTransform(origT);
+            break;
+        case TEXTUAL:
+            g.setColor(Color.BLACK);
+            g.setFont(Rendering.labels);
+            String text;
+            if (value < 10 && value != 0) {
+                text = Float.toString(value);
+                String orig = text;
+                while (text.length() > 3 && g.getFontMetrics().stringWidth(text) > 2 * halfWidth) {
+                    String nline = text.substring(0, text.length() - 1);
+                    try {
+                        if (Float.parseFloat(nline) == 0) {
+                            break;
+                        }
+                    } catch (NumberFormatException ex) {
+                        Logger.warning("Unexpected failure of number formatting", ex);
+                        break;
+                    }
+                    text = nline;
+                }
+                if (g.getFontMetrics().stringWidth(text) > 2 * halfWidth) {
+                    g.setFont(Rendering.console);
+                    text = orig;
+                }
+            } else {
+                text = String.format("%.3f", value);
+            }
+            g.drawString(text, centerX - g.getFontMetrics().stringWidth(text) / 2, centerY + 5);
+            break;
+        case CONFIGURATION: // never called
         }
-        Shape c = g.getClip();
-        g.setClip(new Rectangle(centerX - halfWidth + 10, centerY - halfHeight / 2, halfWidth * 2 - 20, halfHeight));
-        g.fillRect(ptrCtr - 5, centerY - halfHeight / 2 + 1, 11, halfHeight / 2 - 4);
-        g.fillPolygon(new int[] { ptrCtr - 5, ptrCtr, ptrCtr + 6 }, new int[] { centerY - 3, centerY + 3, centerY - 3 }, 3);
-        g.setClip(c);
     }
 
     @Override

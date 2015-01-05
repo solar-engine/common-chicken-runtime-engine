1e47aebe1e3ba5cfb65ad0bc5df89efa357428f4
Colby Skeggs <skeggsc@catlin.edu>
2015-01-04 17:29:13 -0800

    Added SuppressWarnings to allow for warning suppression.

---
+++ CCRE_Igneous_cRIO/src/java/lang/SuppressWarnings.java
@@ -0,0 +1,14 @@
+package java.lang;
+
+/**
+ * This a substitute for java.lang.SuppressWarnings for Squawk. Does nothing useful whatsoever except stuff can work.
+ *
+ * @see java.lang.Override
+ * @author skeggsc
+ */
+public @interface SuppressWarnings {
+
+    @SuppressWarnings("javadoc")
+    String[] value();
+
+}

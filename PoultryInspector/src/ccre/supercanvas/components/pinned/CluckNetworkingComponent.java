e42b2a223169861d4c8fa6e456b8ecae2faae76b
Colby Skeggs <skeggsc@catlin.edu>
2015-01-06 17:19:49 -0800

    Fixed PoultryInspector default USB address.

--- PoultryInspector/src/ccre/supercanvas/components/pinned/CluckNetworkingComponent.java
+++ PoultryInspector/src/ccre/supercanvas/components/pinned/CluckNetworkingComponent.java
@@ -49,7 +49,7 @@ public class CluckNetworkingComponent extends SuperCanvasComponent {
             "roboRIO (alternate 1)", "roboRIO (alternate 2)", "Local (alternate 1)", "Local (alternate 2)" };
     private static final String[] optionAddrs = new String[] {
             "10.$T$E.$A$M.2:443", "roboRIO-$T$E$A$M.local:5800", "127.0.0.1:1540",
-            "177.22.11.2:1540", "roboRIO-$T$E$A$M.local:1540", "10.$T$E.$A$M.2:1540", "10.$T$E.$A$M.2:80",
+            "172.22.11.2:1540", "roboRIO-$T$E$A$M.local:1540", "10.$T$E.$A$M.2:1540", "10.$T$E.$A$M.2:80",
             "roboRIO-$T$E$A$M.local:5805", "roboRIO-$T$E$A$M.local:1735", "127.0.0.1:80", "127.0.0.1:443" };
 
     private final StringBuilder address = new StringBuilder(optionAddrs[0]);

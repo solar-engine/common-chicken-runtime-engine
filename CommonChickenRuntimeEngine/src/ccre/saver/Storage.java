fd9f1270116dd002b665668d1ecebd542fd69e7f
Colby Skeggs <skeggsc@catlin.edu>
2015-09-05 00:29:04 -0700

    Oops - forgot these earlier.

---
+++ CommonChickenRuntimeEngine/src/ccre/saver/Storage.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2013-2014 Colby Skeggs
+ *
+ * This file is part of the CCRE, the Common Chicken Runtime Engine.
+ *
+ * The CCRE is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * The CCRE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the CCRE.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ccre.saver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * The holder for the current storage provider, and the superclass for any
+ * storage providers.
+ *
+ * @author skeggsc
+ */
+public class Storage {
+
+    private static File basedir = new File(".");
+
+    /**
+     * Set the backing folder.
+     *
+     * @param basedir the directory to store CCRE data in.
+     */
+    public static void setBaseDir(File basedir) {
+        Storage.basedir = basedir;
+    }
+
+    /**
+     * Open an output stream to the specified file.
+     *
+     * @param name The filename.
+     * @return The output stream to that file.
+     * @throws java.io.IOException If an error occurs while opening an output.
+     */
+    public static OutputStream openOutput(String name) throws IOException {
+        return new FileOutputStream(new File(basedir, name));
+    }
+
+    /**
+     * Open an input stream from the specified file, or NULL if it doesn't
+     * exist.
+     *
+     * @param name The filename.
+     * @return The input stream from the file, or null if it doesn't exist.
+     * @throws java.io.IOException If an error occurs while opening the file
+     * besides the file not existing.
+     */
+    public static InputStream openInput(String name) throws IOException {
+        try {
+            return new FileInputStream(new File(basedir, name));
+        } catch (FileNotFoundException ex) {
+            return null;
+        }
+    }
+
+    /**
+     * Open a StorageSegment for the specified name.
+     *
+     * @param name the name of the storage segment.
+     * @return the StorageSegment that has been opened.
+     */
+    public static StorageSegment openStorage(String name) {
+        return new StorageSegment(name);
+    }
+}
---
+++ CommonChickenRuntimeEngine/src/ccre/util/CallerInfo.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2014-2015 Colby Skeggs
+ *
+ * This file is part of the CCRE, the Common Chicken Runtime Engine.
+ *
+ * The CCRE is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * The CCRE is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the CCRE.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ccre.util;
+
+/**
+ * A class representing information about the caller of a method. This is
+ * converted into by the appropriate local platform.
+ *
+ * @author skeggsc
+ */
+public class CallerInfo {
+    private final String className;
+    private final String methodName;
+    private final String fileName;
+    private final int lineNum;
+
+    /**
+     * Create a new CallerInfo with the specified pieces of information.
+     *
+     * Only className is required - the rest can be set to null or -1.
+     *
+     * @param className the name of the class, required. This will be normalized
+     * into dot-form.
+     * @param methodName the name of the method, optional.
+     * @param fileName the name of the file, optional.
+     * @param lineNum the line number, optional. (Set to a negative number for
+     * unspecified.)
+     * @throws IllegalArgumentException if className is null.
+     */
+    public CallerInfo(String className, String methodName, String fileName, int lineNum) {
+        if (className == null) {
+            throw new IllegalArgumentException();
+        }
+        this.className = className.replace('/', '.');
+        this.methodName = methodName;
+        this.fileName = fileName;
+        this.lineNum = lineNum;
+    }
+
+    /**
+     * @return the name of the class, in dot-form.
+     */
+    public String getClassName() {
+        return className;
+    }
+
+    /**
+     * @return the name of the method, or null if not known.
+     */
+    public String getMethodName() {
+        return methodName;
+    }
+
+    /**
+     * @return the name of the file, or null if not known.
+     */
+    public String getFileName() {
+        return fileName;
+    }
+
+    /**
+     * @return the line number, or a negative number if not known.
+     */
+    public int getLineNum() {
+        return lineNum;
+    }
+
+    /**
+     * Describe the CallerInfo with a basic string.
+     *
+     * CallerInfo returns a string in the form of
+     * "[CLASSNAME].[METHODNAME]([FILENAME]:[LINENUM])", where each "[VAR]" is
+     * replaced with the corresponding value. CLASSNAME is in dot-form.
+     *
+     * @return the generated string.
+     */
+    public String toString() {
+        return className + "." + methodName + "(" + getFileName() + ":" + lineNum + ")";
+    }
+}

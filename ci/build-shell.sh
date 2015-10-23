1b67b21126a9468cbb2962e3d81a3862c12d438d
Colby Skeggs <skeggsc@catlin.edu>
2015-10-23 07:52:44 -0700

    Added code to display the errors and output from unit tests.

--- ci/build-shell.sh
+++ ci/build-shell.sh
@@ -9,28 +9,29 @@ do
 	ant
 done
 
+dump_logs() {
+	ls $HERE/junit-output
+	echo
+	for file in $HERE/junit-output/*
+	do
+		echo "====> $file"
+		cat $file
+	done
+}
+
 cd $(dirname $HERE)/CommonChickenRuntimeEngine
 rm -rf $HERE/junit-output
 mkdir $HERE/junit-output
-#if
-ant test -Djunit.dir=$HERE -Djunit-output.dir=$HERE/junit-output
-#then
-#	echo "Success!"
-#else
-#	echo "Failure!"
-#	exit 1
-#fi
-
-#	ls $HERE/junit-output
-#	echo
-#	for file in $HERE/junit-output/*
-#	do
-#		echo "====> $file"
-#		cat $file
-#	done
-
+if ant test -Djunit.dir=$HERE -Djunit-output.dir=$HERE/junit-output
+then
+	dump_logs
+	echo "Success!"
+else
+	dump_logs
+	echo "Failure!"
+	exit 1
+fi
 
 cd $HERE
 
 echo "Done!"
-

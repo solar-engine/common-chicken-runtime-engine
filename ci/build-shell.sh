715f24686078f40240f360327ee0393ca5842474
Colby Skeggs <skeggsc@catlin.edu>
2015-11-02 15:51:31 -0800

    Removed log dumping.

--- ci/build-shell.sh
+++ ci/build-shell.sh
@@ -17,16 +17,6 @@ do
 	ant
 done
 
-dump_logs() {
-	ls $HERE/junit-output
-	echo
-	for file in $HERE/junit-output/*
-	do
-		echo "====> $file"
-		cat $file
-	done
-}
-
 echo Testing...
 
 cd $(dirname $HERE)/CommonChickenRuntimeEngine
@@ -34,10 +24,8 @@ rm -rf $HERE/junit-output
 mkdir $HERE/junit-output
 if ant test -Djunit.dir=$HERE -Djunit-output.dir=$HERE/junit-output
 then
-	dump_logs
 	echo "Success!"
 else
-	dump_logs
 	echo "Failure!"
 	exit 1
 fi
